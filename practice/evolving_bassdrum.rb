d0baf01c38e8c6775b5e982eb74b462a3a8b8597
Stephan Muller <stephan@frontmen.nl>
2018-04-14 23:24:21 +0200

    Optimizing randomized bassdrum

--- practice/evolving_bassdrum.rb
+++ practice/evolving_bassdrum.rb
@@ -5,18 +5,19 @@ use_bpm 100
 x = []
 
 live_loop :count do
-  x = spread(rrand(3,6),8).ring.shuffle.dup
+  r = rrand(3, 6)
+  x = (knit 1, r, 0, 8-r).shuffle
   sleep 16
-end
-
-live_loop :hihat do
-  sample :drum_cymbal_closed, ampp: 0.7
-  sleep 0.25
 end
 
 live_loop :bass do
   sample :drum_bass_hard, amp: 3, on: x.tick
   sleep 0.5
+end
+
+live_loop :hihat do
+  sample :drum_cymbal_closed, ampp: 0.7
+  sleep 0.25
 end
 
 live_loop :snare do
