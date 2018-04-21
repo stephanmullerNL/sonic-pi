a9a003fcd65996f8e3065f6cdaa85f41a4d1d792
Stephan Muller <stephan@frontmen.nl>
2018-04-22 01:19:35 +0200

    More samples

--- utils/all_samples.rb
+++ utils/all_samples.rb
@@ -7,10 +7,16 @@ bass = [:bass_dnb_f, :bass_drop_c, :bass_hard_c,
         :bass_hit_c, :bass_thick_c, :bass_trance_c,
         :bass_voxy_c, :bass_voxy_hit_c, :bass_woodsy_c]
 
-samples = bass
+bd = [:bd_808, :bd_ada, :bd_boom, :bd_fat, :bd_gas,
+      :bd_haus, :bd_klub, :bd_mehackit, :bd_pure,
+      :bd_sone, :bd_tek, :bd_zome, :bd_zum]
+
+samples = bd
 
 live_loop :samples do
   current = samples.ring.tick
-  sample current
-  sleep sample_duration current
+  8.times do
+    sample current, amp: 4
+    sleep sample_duration current
+  end
 end
