620ab2f674d2d8cbfde07711913e8c340a351221
Stephan Muller <stephan@frontmen.nl>
2018-05-01 22:15:36 +0200

    Worked on both tracks

--- tracks/track1.rb
+++ tracks/track1.rb
@@ -1,6 +1,3 @@
-
-use_random_seed 3003001
-
 ### DRONE
 
 drone_fx = :pan
@@ -14,16 +11,19 @@ live_loop :drones do
   end
 end
 
+##| sleep 8
+
 ### DRUMS
 
 buildup = (ramp *range(0, 2, 0.02))
 dens = (knit 4, 1, 1, 7, 3, 1, 1, 7).ring
+
 live_loop :kick do
   tick
   density dens.look do
     with_fx :slicer, phase: 0.1 do
       with_fx :distortion, distort: 0.8 do
-        sample :bd_klub, amp: buildup.look, rate:3, hpf: 70
+        sample :bd_klub, amp: buildup.look, rate: 3, hpf: 70
         sleep 0.5
       end
     end
@@ -33,40 +33,42 @@ end
 ##| sleep 32
 
 live_loop :beat, sync: :kick do
-  with_fx :flanger, phase:2 do
-    # TODO: fix distortion
-    sample :loop_breakbeat, beat_stretch: 2, pre_amp: 3
-    sleep 2
-  end
+  sample :loop_breakbeat, beat_stretch: 2, pre_amp: 3
+  sleep 2
 end
 
 drone_fx = :slicer
 
 ##| sleep 32
 
-
+use_random_seed 1343
 chords = (chord :e2, :major, num_octaves: 3).shuffle.take(16)
 
+melodies = [
+  [67, 64, 43, 48, 52, 60, 40, 36], 8,
+  [67, 48, 28, 60, 55, 52, 36, 43, 40, 24, 64, 31, 67, 48, 28, 60], 32,
+  [75, 56, 36, 68, 63, 60, 44, 51, 48, 32, 72, 39, 75, 56, 36, 68], 8,
+  [67, 48, 28, 60, 55, 52, 36, 43, 40, 24, 64, 31, 67, 48, 28, 60], 24,
+  [59, 40, 20, 52, 47, 44, 28, 35, 32, 16, 56, 23, 59, 40, 20, 52], 8,
+  [67, 48, 28, 60, 55, 52, 36, 43, 40, 24, 64, 31, 67, 48, 28, 60], 24,
+  [44, 59, 52, 64, 56, 47, 71, 40, 68, 44, 59, 52, 64, 56, 47, 68], 8
+]
 
-melody = []
-melodies = (ramp
-            [67, 64, 43, 48, 52, 60, 40, 36],
-            [67, 48, 28, 60, 55, 52, 36, 43, 40, 24, 64, 31, 67, 48, 28, 60],
-            chords
-            )
+melody = melodies[0].ring
 
 live_loop :melody do
   melody = melodies.tick.ring
-  16.times {puts melody}
-  sleep melody.length
+  sleep melodies.tick
 end
 
 live_loop :music, sync: :kick do
   tick
-  with_fx :bitcrusher, bits: 8 do
-    with_synth :dsaw do
-      play melody.look, release: 0.125, lpf: 105, hpf: 110
-      sleep 0.125
+  with_fx :octaver, mix: 0.5 do
+    with_fx :distortion, distort: 0 do
+      with_synth :piano do
+        play melody.look, release: 0.125
+        sleep 0.125
+      end
     end
   end
 end
--- tracks/track2.rb
+++ tracks/track2.rb
@@ -1,5 +1,13 @@
 use_bpm 70
 
+doop = (knit :d2, 4, :f2, 3, :e2, 1).ring
+blips = (ring 63, 67, 60)
+chitz = (ring 63, 62, 67, 61, 0, 65, 0, 0, 62, 66, 67, 65, 0, 61, 0, 0)
+
+16.times do
+  puts chitz
+end
+
 live_loop :beat do
   sleep 0.25
   sample :drum_cymbal_closed
@@ -7,13 +15,12 @@ live_loop :beat do
   sample :drum_bass_hard
 end
 
-notes = (knit :d2, 4, :f2, 3, :e2, 1).ring
-
 live_loop :doop, sync: :beat do
   with_fx :distortion do
     with_fx :flanger do
       use_synth :dsaw
-      play notes.tick, release: 0.20, attack: 0, cutoff: 60
+      play notes = (knit :d2, 4, :f2, 3, :e2, 1).ring
+      .tick, release: 0.20, attack: 0, cutoff: 60
       sleep 0.5
     end
   end
@@ -23,7 +30,7 @@ sleep 16
 
 live_loop :crash do
   with_fx :krush, res: 0.7 do
-    sample :drum_cowbell, cutoff: 90, amp: cow_amp
+    sample :drum_cowbell, cutoff: 90, amp: 1
     sleep 4
   end
 end
@@ -37,17 +44,36 @@ live_loop :drums, sync: :beat do
 end
 
 sleep 16
-s = ((knit :rest, 5)  + [63, 67, 60] + (knit :rest, 8)).ring
 
 live_loop :blips, sync: :beat do
   with_fx :distortion, distort: 0.2 do
     with_fx :bitcrusher do
       with_synth :prophet do
-        play s.tick, release: 0.125
-        sleep 0.25
+        
+        sleep 1.25
+        
+        3.times do
+          play blips.tick, release: 0.125
+          sleep 0.25
+        end
+        
+        sleep 2
       end
     end
   end
 end
 
 sleep 16
+
+live_loop :chitz, sync: :beat do
+  chitz_vol_1 = rrand_i(0, 1)
+  chitz_vol_2 = rrand_i(0, 1)
+  32.times do
+    tick
+    with_synth :dsaw do
+      play chitz.look, release: 0.2, cutoff: 80, amp: 0.5 * chitz_vol_1
+      sample :elec_bong, rpitch: chitz.look - 60, amp: 1 * chitz_vol_2
+      sleep 0.25
+    end
+  end
+end
