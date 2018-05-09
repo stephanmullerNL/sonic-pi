c93fb4288a95adb4ca745305e4d5f51025db40c0
Stephan Muller <stephan@frontmen.nl>
2018-05-09 15:12:33 +0200

    Rewrote tab machine

--- utils/tab-machine.rb
+++ utils/tab-machine.rb
@@ -1,51 +1,59 @@
 use_bpm 100
+time = 0.25
 
-# INSTRUMENTS
-bass = :drum_bass_hard
-cymbal = :drum_cymbal_open
-hihat = :drum_cymbal_closed
-snare = :drum_snare_hard
-tom1 = :drum_tom_hi_hard
-tom3 = :drum_tom_lo_hard
-
-# TABS
 tabs = "
- S:|----o--o-o--o---|----o--o-o--o---|----o--o-o--o---|----o--o-o--o---|
- B:|o--o----o-oo--o-|o--o----o-oo--o-|o--o----o-oo--o-|o--o----o-oo--o-|
-
  C:|X---------------|X---------------|X---------------|X---------------|
 HH:|----o---o---o---|----o---o---o---|----o---o---o---|----o---o-------|
  S:|----o--o-o--o---|----o--o-o--o---|----o--o-o--o---|----o-----oooooo|
- B:|o--o----o-oo--o-|o--o----o-oo--o-|o--o----o-oo--o-|o--o--o-o-------|"
+ B:|o--o----o-oo--o-|o--o----o-oo--o-|o--o----o-oo--o-|o--o--o-o-------|
 
-define :drum do | instrument, note |
-  sample instrument if note && note != '-'
-end
+HH:|x-x-x-x-x-x-x-x-|x-x-x-x-x-x-x-o-|x-x-x-x-x-x-x-x-|x-x-x-x-x-x-x-o-|
+ S:|----o-------o---|----o-------o---|----o-------o---|----o-------o---|
+ B:|o-o-----o-o-----|o-o-----o-o-----|o-o-----o-o-----|o-o-----o-o-----|
+"
 
-def parse(tabs)
-  return tabs.split(/\n/)
-  .select { | item | item.length > 0 }
-  .map { | str | str.strip || str }
-  .inject (Hash.new('')) do | all, current |
-    char, rest = current.split(':')
-    puts all, char, rest
+instruments = {
+  'B' => { 'o' => :drum_bass_hard },
+  'C' => { 'X' => :drum_splash_hard },
+  'HH' => {
+    'x' => :drum_cymbal_closed,
+    'o' => :drum_cymbal_open
+  },
+  'S' => {
+    'o' => :drum_snare_hard,
+    'f' => :drum_snare_hard
+  },
+  'T' => { 'o' => :drum_tom_hi_hard },
+  'FT' => { 'o' => :drum_tom_lo_hard }
+}
+
+# convert an array of tab lines to an object the machine can work with
+def to_bar_hash(ar)
+  ar.inject (Hash.new('')) do | all, current |
+    char, rest = current.tr(' |', '').split(':')
     all[char] += rest
     all
   end
 end
 
-tabs = parse(tabs)
+# split tabs into separate bars
+def parse(tabs)
+  return tabs.split(/\n/)
+  .chunk {  | line | line.include? '|' }
+  .select { | k, v | v[0].include? '|' }
+  .map { | k, v | to_bar_hash(v) }
+end
 
-puts tabs
+bars = parse(tabs)
 
-live_loop :machine do
-  tick
-  drum bass, tabs['B'].tr('|', '')[look]
-  drum cymbal, tabs['C'].tr('|', '')[look]
-  drum hihat, tabs['HH'].tr('|', '')[look]
-  drum snare, tabs['S'].tr('|', '')[look]
-  drum tom1, tabs['T'].tr('|', '')[look]
-  drum tom3, tabs['FT'].tr('|', '')[look]
-  sleep 0.25
-  tick_reset if look > tabs['B'].length
+bars.each do | bar |
+  # Assume each line in a bar is as long as the first one
+  length = bar.first[1].length
+  length.times do | n |
+    instruments.each do | key, sounds |
+      note = bar[key][n]
+      sample sounds[note]
+    end
+    sleep time
+  end
 end
