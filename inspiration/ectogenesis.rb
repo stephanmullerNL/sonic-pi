60264174975a63421bdf71e81bd333c3fb6c4b7d
Stephan Muller <stephan@frontmen.nl>
2018-05-10 21:36:51 +0200

    Improved ectogenesis

--- inspiration/ectogenesis.rb
+++ inspiration/ectogenesis.rb
@@ -1,25 +1,40 @@
+#Animals as Leaders - Ectogenesis
+
 use_bpm 92
 
-notes = [30, 30, 33, 30, 32, 30, 35, 30, 33, 30, 37, 33, 35, 32, 30, 30, 30, 33, 30, 32, 30, 35, 30, 33, 30, 37, 33, 35, 32].ring
-pause = (knit 0, 11, 1, 1, 0, 1, 1, 1, 0, 12, 1, 1, 0, 1, 1, 1).ring
-
-16.times { puts notes.length }
-
-live_loop :tick do
-  sample :drum_cymbal_closed
-  sleep 0.8
-end
+notes =  [30, 30, 33, 30, 32, 30, 35, 30, 33, 30, 37, 33, 30, 35, 32, 30].ring
+chimes = [14, 16, 26, 27, 30, 41, 43, 46, 56, 57, 59, 61, 62, 64]
+drums =  [56, 57, 59, 61, 62, 64]
 
 live_loop :bassline do
-  tick
-  
-  ##| with_fx :distortion do
-  use_synth :blade
-  play notes.look, release: 0.2
-  16.times do
-    puts notes.look if pause.look > 0
-  end
-  
+  synth :fm, note: notes.tick + 12, release: 0, attack: 0, sustain: 0.15, amp: 2
   sleep 0.2
-  sleep 0.1  if pause.look > 0
+end
+
+live_loop :intro do
+  tick
+  with_fx :bitcrusher do
+    with_fx :distortion do
+      sample :ambi_choir, attack: 0, sustain: 0.1, release: 0.1, on: (chimes.include? look + 1), start: 0.1
+    end
+  end
+  sample :drum_snare_hard, on: (drums.include? look + 1)
+  sleep 0.2
+  ##| tick_reset if look > 64
+end
+
+sleep 12.8
+
+live_loop :drums do
+  sample :drum_bass_hard, amp: 2, on: (pattern('x-----x--------x') && (tick + 1) % 32)
+  sample :drum_snare_hard, on: pattern('--------x-------')
+  sample :drum_cymbal_closed, on: pattern('xx-x-xxx-xx-xx-x')
+  sleep 0.2
+end
+
+
+
+
+def pattern(p)
+  return p.ring.tick(p) == 'x'
 end
