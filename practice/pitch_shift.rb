a368d7e4d91bc66541d4a848ddac5fff483b12e4
Stephan Muller <stephan@frontmen.nl>
2018-05-12 13:23:01 +0200

    Figured out how to do a proper pitch shift using control

--- practice/pitch_shift.rb
+++ practice/pitch_shift.rb
@@ -2,22 +2,11 @@ pitcher = range(0, 5, 0.1)
 p = pitcher[0]
 use_synth :dsaw
 
-live_loop :pitch do
-  tick_reset
-  p = pitcher[0]
-  play 40, sustain: 1, attack: 0, release: 0, pitch: p
-  sleep 1
-  32.times do
-    p = pitcher.tick 
-    play 40, sustain: 0.02, attack: 0, pitch: p
-    sleep 0.02
-  end
-  play 40, sustain: 2, attack: 0, release: 0, pitch: p
-  sleep 2
-end
-
 live_loop :note do
-  use_synth :dsaw
-  ##| play 40, sustain: 4, pitch: p
-  sleep 4
+  with_fx :pitch_shift, pitch_slide: 1 do | fx |
+    note = play 40, sustain: 4
+    sleep 1
+    control fx, pitch: 5
+    sleep 3
+  end
 end
