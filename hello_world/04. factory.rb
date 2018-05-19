cb8e6fb1e7913d8f0f6c1eee0f77fd5958b1bc4e
Stephan Muller <stephan@frontmen.nl>
2018-05-19 22:33:57 +0200

    More updates

--- hello_world/04. factory.rb
+++ hello_world/04. factory.rb
@@ -1,30 +1,25 @@
 use_bpm 40
-
 bla = '~/s/newshit/blabla.wav'
+s = 1
 
 def pattern(p)
   return p.ring.tick(p) == 'x'
 end
+
 
 live_loop :brr do
   sample :glitch_bass_g
   sleep 2
 end
 
-scales = [:c3, :d3, :b3]
-live_loop :zim, sync: :brr do
-  sc = scales.choose
-  4.times do
-    play_chord (scale sc, :bhairav), sustain: 2, release: 2
-    sleep 4
-  end
-end
+sleep 4 * s
 
-live_loop :percussion do
-  sample :glitch_perc4, amp: 1, on: pattern('x---')
-  sample :glitch_perc2, on: pattern('x----x--')
-  sample :elec_blup, on: pattern('--x-')
-  sleep 0.25
+live_loop :toms do
+  5.times do
+    sample :drum_tom_mid_soft
+    sleep 0.1
+  end
+  sleep 12.5
 end
 
 live_loop :chaka do
@@ -38,22 +33,27 @@ live_loop :chaka do
   sleep 3
 end
 
-notes = [54, 55, 56, 57].ring
-live_loop :boop do
-  use_synth :beep
-  with_fx :reverb, room: 0.4 do
-    ##| play notes.tick, release: 0.2, on: pattern('xx-xx-xx'), lpf: 70
-    sleep 0.25
+sleep 16 * s
+
+live_loop :percussion do
+  sample :glitch_perc4, amp: 1, on: pattern('x---')
+  sample :glitch_perc2, on: pattern('x----x--')
+  sample :elec_blup, on: pattern('--x-')
+  sleep 0.25
+end
+
+
+scales = [:c3, :d3, :f3, :d3].ring
+live_loop :zim, sync: :brr do
+  sc = scales.tick
+  use_synth :mod_beep
+  4.times do
+    play_chord (scale sc, :gong), sustain: 2, release: 2
+    sleep 4
   end
 end
 
-live_loop :toms do
-  5.times do
-    sample :drum_tom_mid_soft
-    sleep 0.1
-  end
-  sleep 12.5
-end
+sleep 4 * s
 
 live_loop :sprinkle do
   6.times do
@@ -63,10 +63,23 @@ live_loop :sprinkle do
   sleep 4.25
 end
 
+sleep 4 * s
+
 onsets = [1, 3, 24, 26].ring
 live_loop :bla, sync: :brr do
   with_fx :echo, decay: 4 do
     sample bla, onset: onsets.tick, amp: 2
     sleep 8
+  end
+end
+
+
+
+notes = [54, 55, 56, 57].ring
+live_loop :boop do
+  use_synth :beep
+  with_fx :reverb, room: 0.4 do
+    ##| play notes.tick, release: 0.2, on: pattern('xx-xx-xx'), lpf: 70
+    sleep 0.25
   end
 end
